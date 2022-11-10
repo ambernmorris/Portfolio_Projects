7aeace87b27ae2485593aeb305eb9feb62021f73
ambernmorris <113140914+ambernmorris@users.noreply.github.com>
2022-11-10 15:16:55 -0500

    Update Covid Portfolio Project SQL.sql

--- Covid Portfolio Project SQL.sql
+++ Covid Portfolio Project SQL.sql
@@ -1,3 +1,7 @@
+Used SQL to perform some exploration on global data of confirmed COVID-19 deaths from February 2020 to April 2021.
+Original data set: https://ourworldindata.org/covid-deaths.
+Research and data collection done by: https://ourworldindata.org/team
+
 SELECT *
 FROM PortfolioProject..CovidDeaths
 order by 3,4
