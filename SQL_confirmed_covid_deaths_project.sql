dfd686db261fc0c0b3faa8630e079f8a18c66f28
ambernmorris <113140914+ambernmorris@users.noreply.github.com>
2023-07-31 10:56:18 -0400

    Update SQL_confirmed_covid_deaths_project.sql

--- SQL_confirmed_covid_deaths_project.sql
+++ SQL_confirmed_covid_deaths_project.sql
@@ -1,4 +1,5 @@
 Used SQL to perform some exploration on global data of confirmed COVID-19 deaths from February 2020 to April 2021.
+	
 Original data set: https://ourworldindata.org/covid-deaths.
 Research and data collection done by: https://ourworldindata.org/team
 
